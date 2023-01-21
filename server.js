42a347f1ab58606ee5835d2889793daa6f184547
GitOfAMN <ashleynickens@gmail.com>
2023-01-20 21:36:52 -0500

    ch5 folder

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ app.get('/blog', (req, res) => {
     res.json(mockBlog);
 });
 
+//create
+app.post('/blog', (req, res) => {
+    let newBlog = {
+        id: mockBlog.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    mockBlog.push(newBlog)
+    res.json(newBlog);
+});
+
+//show
+app.get('/blog/:id', (req, res) => {
+    let blog = mockBlog.filter(b => b.id == req.params.id)
+    res.json(blog[0]);
+});
+
+//middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 app.listen(3008, () => {
     console.log('Server is running on port 3008');
 });
